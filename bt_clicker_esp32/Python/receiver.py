b345c252cc061ac0247c280416502f8e84d95995
James Watson <james.watson-2@colorado.edu>
2025-05-03 19:52:31 -0600

    Found yt corner from screenshot!

--- bt_clicker_esp32/Python/receiver.py
+++ bt_clicker_esp32/Python/receiver.py
@@ -1,17 +1,75 @@
 # https://claude.ai/share/99f4f95b-8539-48d7-9684-fdddfc04d2c5
 # python3.11 -m pip install selenium --user
 
+########## INIT ####################################################################################
 import simplepyble
 import pynput
 from pynput.mouse import Button
 from pynput.keyboard import Key
 from time import sleep
+import numpy as np
+from PIL import ImageGrab
 
 # UUID for the service and characteristic that send our signal
 # These should match the UUIDs in your ESP32 code
 SERVICE_UUID        = "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
 CHARACTERISTIC_UUID = "beb5483e-36e1-4688-b7f5-ea07361b26a8"
+_START_LOC          = [ 592,  732 ] # Image Space
+_AD_OFFSET          = [ -80, -120 ] # Mouse Space
 
+
+########## SCREEN IMAGE ANALYSIS ###################################################################
+
+def get_image():
+    printscreen_pil   = ImageGrab.grab( all_screens = True )
+    # printscreen_pil.show()
+    printscreen_numpy = np.array( printscreen_pil.getdata(), dtype='uint8' ).reshape(
+        (printscreen_pil.size[1], printscreen_pil.size[0], 3,)
+    )
+    return printscreen_numpy
+
+
+def get_swatch_avg_value( img : np.ndarray, x0y0, x1y1 ):
+    swatch = img[ x0y0[0]:x1y1[0], x0y0[1]:x1y1[1], : ]
+    rtnVal = np.sum( swatch ) / swatch.size
+    # print( rtnVal )
+    return rtnVal
+
+
+def get_video_bbox( img, startLoc, swatchWidth = 40, swatchThick = 4, thresh = 255*0.95 ):
+    """ Attempt to find the edges of the video """
+    hWidth = int( swatchWidth / 2 )
+    incr   = 1 #swatchThick
+    xBox   = [[startLoc[0], startLoc[1]-hWidth], [startLoc[0]+swatchThick, startLoc[1]+hWidth,],]
+    yBox   = [[startLoc[0]-hWidth, startLoc[1],], [startLoc[0]+hWidth, startLoc[1]+swatchThick,],]
+    vals   = [0.0, 0.0,]
+    corner = startLoc
+    while vals[0] < thresh:
+        vals[0] = get_swatch_avg_value( img, xBox[0], xBox[1] )
+        if vals[0] >= thresh:
+            corner[0] = xBox[1][0]
+        else:
+            xBox[0][0] += incr
+            xBox[1][0] += incr
+    while vals[1] < thresh:
+        vals[1] = get_swatch_avg_value( img, yBox[0], yBox[1] )
+        if vals[1] >= thresh:
+            corner[1] = yBox[1][1]
+        else:
+            yBox[0][1] += incr
+            yBox[1][1] += incr
+    return corner
+
+
+def get_yt_bbox():
+    """ Get the bounding box of the YT video """
+    img  = get_image()
+    crnr = get_video_bbox( img, _START_LOC, swatchWidth = 40, swatchThick = 4, thresh = 255*0.999 )
+    return [crnr[1], crnr[0]] # Image space to mouse space
+
+
+
+########## MAIN ####################################################################################
 def main():
     # Initialize controller
     mouse    = pynput.mouse.Controller()
@@ -102,9 +160,14 @@ def main():
             signal = bytes( data ).decode( 'utf-8' ).strip()
             print( f"Received: {signal}, The current mouse position is: {mouse.position}" )
             # https://nitratine.net/blog/post/simulate-keypresses-in-python/#pressing-and-releasing-keys
+
             if signal == "CLICK":
-                print( "Performing mouse click" )
+                corner = get_yt_bbox()
+                nuPosn = [int(item) for item in np.add( corner, _AD_OFFSET ).tolist()]
+                print( f"Sending mouse to: {nuPosn}" )
+                mouse.position = nuPosn
                 mouse.click( Button.left )
+
             elif signal == "FWD":
                 print( "Performing right arrow" )
                 short_kb_press( Key.right, _PRESS_TIME_S )
